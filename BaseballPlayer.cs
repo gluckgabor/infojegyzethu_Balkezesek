8962b35ea3dfc877f29677c162644ce2556768c8
Gábor Glück <gluckgabor@gmail.com>
2020-11-15 23:49:00 +0100

    Add project files.

---
+++ BaseballPlayer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Balkezesek
+{
+    internal class BaseballPlayer
+    {
+        public string nev { get; set; }
+        public DateTime elso { get; set; }
+        public DateTime utolso { get; set; }
+        public double suly { get; set; }
+        public double magassag { get; set; }
+
+        public BaseballPlayer(string line)
+        {
+            string[] lineSplitted = line.Split(";");
+            string[] elsoDateSplitted = lineSplitted[1].Split("-");
+            string[] utolsoDateSplitted = lineSplitted[2].Split("-");
+
+            nev = lineSplitted[0];
+            elso = new DateTime(Convert.ToInt32(elsoDateSplitted[0]), Convert.ToInt32(elsoDateSplitted[1]), Convert.ToInt32(elsoDateSplitted[2]));
+            utolso = new DateTime(Convert.ToInt32(utolsoDateSplitted[0]), Convert.ToInt32(utolsoDateSplitted[1]), Convert.ToInt32(utolsoDateSplitted[2]));
+            suly = Convert.ToDouble(lineSplitted[3]);
+            magassag = Convert.ToDouble(lineSplitted[4]);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Balkezesek
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            
+            string[] lines = File.ReadAllLines("C:/Users/gluck/source/repos/Balkezesek/balkezesek.csv");
+
+            List<BaseballPlayer> baseballPlayersList = new List<BaseballPlayer>();
+
+            for (int i = 1; i < lines.Length; i++)
+            {
+                BaseballPlayer baseballPlayer = new BaseballPlayer(lines[i]);
+
+                baseballPlayersList.Add(baseballPlayer);
+            }           
+
+            Console.WriteLine("3. feladat:");
+            numberOfLines(baseballPlayersList);
+
+            Console.WriteLine("4. feladat:");
+            nameHeightLastPlayedInOct99(baseballPlayersList);
+
+            Console.WriteLine("5. feladat:");
+
+           averageWeightOfPlayersPlayedInYear(baseballPlayersList, yearQuery(baseballPlayersList));
+        }
+
+        private static void averageWeightOfPlayersPlayedInYear(List<BaseballPlayer> baseballPlayersList, DateTime queriedDate)
+        {
+            Console.WriteLine("6. feladat:");
+            double averageWeightDividend = 0;
+            int averageWeightDivisor = 0;
+            
+            foreach (var baseballPlayer in baseballPlayersList)
+            {
+                if (queriedDate.Year == baseballPlayer.elso.Year || queriedDate.Year == baseballPlayer.utolso.Year || (queriedDate.Year > baseballPlayer.elso.Year && queriedDate.Year < baseballPlayer.utolso.Year))
+                {
+                    averageWeightDividend += baseballPlayer.suly;
+                    averageWeightDivisor++;
+                }
+            }
+
+            Console.WriteLine(string.Format("{0:0.00}", averageWeightDividend/averageWeightDivisor) + " font");
+        }
+
+        private static DateTime yearQuery(List<BaseballPlayer> baseballPlayersList)
+        {
+            DateTime lowerTreshold = new DateTime(1990,01,01);
+            DateTime upperTreshold = new DateTime(1999,12,31);
+            bool queriedDateIsCorrect;
+            DateTime queriedDate;
+
+            do
+            {
+                Console.WriteLine("Kérek egy 1990 és 1999 közötti évszámot: ");
+                queriedDate = Convert.ToDateTime(Console.ReadLine() + ",01,01");
+                
+                if (!(queriedDate >= lowerTreshold && queriedDate < upperTreshold))
+                {
+                    Console.WriteLine("Hibás adat!");
+                    queriedDateIsCorrect = false;
+                }
+                else
+                {
+                    queriedDateIsCorrect = true;
+                }
+                
+
+            } while (!queriedDateIsCorrect);
+
+            return queriedDate;
+        }
+
+        private static void nameHeightLastPlayedInOct99(List<BaseballPlayer> baseballPlayersList)
+        {
+            DateTime dateBeginning = new DateTime(1999,10,01);
+            DateTime dateEnd = new DateTime(1999,10,31);
+            foreach (var baseballPlayer in baseballPlayersList)
+            {
+                if (baseballPlayer.utolso > dateBeginning && baseballPlayer.utolso < dateEnd)
+                {
+                    Console.WriteLine(baseballPlayer.nev + ", " + String.Format("{0:0.0}",baseballPlayer.magassag*2.54) + " cm");
+                }
+            }
+        }
+
+        private static void numberOfLines(List<BaseballPlayer> baseballPlayersList)
+        {
+             Console.WriteLine(baseballPlayersList.Count);
+        }
+    }
+}
